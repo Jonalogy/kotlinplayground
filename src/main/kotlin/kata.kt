d2b4334c2271d7e498a91f59372a1ee899fff68b
jonalogy <jonalogy@gmail.com>
2019-09-04 09:11:52 +0800

    Add comments and links to katas

--- src/main/kotlin/kata.kt
+++ src/main/kotlin/kata.kt
@@ -2,8 +2,17 @@ import kotlin.math.abs
 import kotlin.math.ceil
 
 class Kata {
+    // Return Negative https://www.codewars.com/kata/55685cd7ad70877c23000102
     fun makeNegative(x: Int): Int = -abs(x)
 
-    // Note: If typed as int, any result will be rounded down to the nearest integer!
-    fun century(number: Int) = (number + 99)/100
+    // Century from year https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+    /* Note: If typed as int, any result will be rounded down to the nearest integer! */
+    fun century(number: Int) = (number + 99) / 100
+
+    // The PaperFold sequence https://www.codewars.com/kata/5d26721d48430e0016914faa
+    fun paperFold(): Iterator<Int> {
+        return iterator {
+            yield(1)
+        }
+    }
 }
